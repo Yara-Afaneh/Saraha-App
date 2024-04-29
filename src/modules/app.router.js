5edbf4cfe05eed9ec6615ef3311cc63ac2bb5f6a
Yara-Afaneh <yara.afaneh@gmail.com>
2024-04-29 22:45:14 +0300

    connecting data base

--- src/modules/app.router.js
+++ src/modules/app.router.js
@@ -6,7 +6,14 @@ import userRouter from "./users/users.router.js";
  export const initApp=(app,express)=>{   
     connectDB();
     app.use(express.json());
+    app.get('/',(req,res)=>{
+        return res.status(200).json({message:"welcome"});
+    })
     app.use('/auth',authRouter)
     app.use('/messages',messagesRouter)
     app.use('/user',userRouter)
+    app.get('*',(req,res)=>{
+        return res.status(404).json({message:"page not found"});
+
+    }); 
 }
